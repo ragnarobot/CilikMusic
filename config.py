50e9848eb8874c5db01d51f8ed596e0ed09ba08c
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-16 08:50:01 -0700

    CilikMusic Stable Release

--- config.py
+++ config.py
@@ -1,66 +1,26 @@
-from os import getenv
+##Config
+##CilikMusic
 
+from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
-
-# VARS
-
 get_queue = {}
-BOT_TOKEN = getenv("BOT_TOKEN")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10"))
-ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
+SESSION_NAME = getenv('SESSION_NAME', 'session')
+BOT_TOKEN = getenv('BOT_TOKEN')
+API_ID = int(getenv('API_ID', "16045921"))
+API_HASH = getenv('API_HASH')
+DURATION_LIMIT = int(getenv('DURATION_LIMIT', '60'))
+COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ . , : ; !').split())
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+SUDO_USERS = list(map(int, getenv('SUDO_USERS', '').split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", '-1001649272989'))
+ASS_ID = int(getenv("ASS_ID", '1604165521'))
+OWNER_ID = list(map(int, getenv('OWNER_ID', '1784606556').split()))
+GROUP = getenv("GROUP", None)
+CHANNEL = getenv("CHANNEL", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/grey423/GreyMusikYuki"
-)
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-
-if str(getenv("SUPPORT_CHANNEL")).strip() == "":
-    SUPPORT_CHANNEL = None
-else:
-    SUPPORT_CHANNEL = str(getenv("SUPPORT_CHANNEL"))
-if str(getenv("SUPPORT_GROUP")).strip() == "":
-    SUPPORT_GROUP = None
-else:
-    SUPPORT_GROUP = str(getenv("SUPPORT_GROUP"))
-
-
-if str(getenv("STRING_SESSION1")).strip() == "":
-    STRING1 = str(None)
-else:
-    STRING1 = str(getenv("STRING_SESSION1"))
-
-if str(getenv("STRING_SESSION2")).strip() == "":
-    STRING2 = str(None)
-else:
-    STRING2 = str(getenv("STRING_SESSION2"))
-
-if str(getenv("STRING_SESSION3")).strip() == "":
-    STRING3 = str(None)
-else:
-    STRING3 = str(getenv("STRING_SESSION3"))
-
-if str(getenv("STRING_SESSION4")).strip() == "":
-    STRING4 = str(None)
-else:
-    STRING4 = str(getenv("STRING_SESSION4"))
-
-if str(getenv("STRING_SESSION5")).strip() == "":
-    STRING5 = str(None)
-else:
-    STRING5 = str(getenv("STRING_SESSION5"))
-
-if str(getenv("LOG_SESSION")).strip() == "":
-    LOG_SESSION = str(None)
-else:
-    LOG_SESSION = str(getenv("LOG_SESSION"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/grey423/CilikMusic")
