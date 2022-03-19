73d6f73eceb91f112520f566fb4047da87b26c11
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-18 23:12:47 -0700

    Update playlist.py

--- CilikMusic/plugins/play/playlist.py
+++ CilikMusic/plugins/play/playlist.py
@@ -55,7 +55,7 @@ async def check_playlist(client, message: Message, _):
         count += 1
         msg += f"\n\n{count}- {title[:70]}\n"
         msg += _["playlist_5"].format(duration)
-    link = await Yukkibin(msg)
+    link = await Cilikbin(msg)
     lines = msg.count("\n")
     if lines >= 17:
         car = os.linesep.join(msg.split(os.linesep)[:17])
