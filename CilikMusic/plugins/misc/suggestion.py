2fe11ff1ace127efe3c2e075b5ca29f5800bf28f
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-29 04:12:09 -0700

    Update suggestion.py

--- CilikMusic/plugins/misc/suggestion.py
+++ CilikMusic/plugins/misc/suggestion.py
@@ -15,7 +15,10 @@ import config
 from config import clean
 from strings import get_string
 from CilikMusic import app
-from CilikMusic.utils.database import get_lang, get_served_chats, get_private_served_chats, is_suggestion
+from CilikMusic.utils.database import (get_lang,
+                                       get_private_served_chats,
+                                       get_served_chats,
+                                       is_suggestion)
 
 LEAVE_TIME = config.AUTO_SUGGESTION_TIME
 
