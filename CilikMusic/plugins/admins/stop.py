043664674413ce14fef84b4f0b8fbb229f13ae13
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-29 04:01:55 -0700

    Update stop.py

--- CilikMusic/plugins/admins/stop.py
+++ CilikMusic/plugins/admins/stop.py
@@ -1,12 +1,20 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
 
 from pyrogram import filters
 from pyrogram.types import Message
-from CilikMusic.utils.database import set_loop
 
 from config import BANNED_USERS
 from strings import get_command
 from CilikMusic import app
 from CilikMusic.core.call import Yukki
+from CilikMusic.utils.database import set_loop
 from CilikMusic.utils.decorators import AdminRightsCheck
 
 # Commands
