3dc4919a78c10135b311240825dc0d435d1e6684
Grey <95908917+grey423@users.noreply.github.com>
2023-01-28 15:43:15 +0700

    Update ping.py

--- CilikMusic/plugins/tools/ping.py
+++ CilikMusic/plugins/tools/ping.py
@@ -6,7 +6,8 @@
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
-
+import asyncio
+from random import shuffle
 from datetime import datetime
 
 from pyrogram import filters
@@ -44,3 +45,50 @@ async def ping_com(client, message: Message, _):
             MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
         )
     )
+
+
+
+   
+tagallgcid = []
+
+
+@app.on_message(
+    filters.command(["all", "cancel"], ["/"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+async def tagcom(client, message: Message):
+    if message.command[0] == "all":
+        if message.chat.id in tagallgcid:
+            return
+        tagallgcid.append(message.chat.id)
+        text = message.text.split(None, 1)[1] if len(message.text.split()) != 1 else ""
+        users = [
+            member.user.mention
+            async for member in message.chat.get_members()
+            if not (member.user.is_bot or member.user.is_deleted)
+        ]
+        shuffle(users)
+        m = message.reply_to_message or message
+        for output in [users[i : i + 5] for i in range(0, len(users), 5)]:
+            if message.chat.id not in tagallgcid:
+                break
+            await asyncio.sleep(1.5)
+            await m.reply_text(
+                ", ".join(output) + "\n\n" + text, quote=bool(message.reply_to_message)
+            )
+        try:
+            tagallgcid.remove(message.chat.id)
+        except Exception:
+            pass
+    elif message.command[0] == "cancel":
+        if message.chat.id not in tagallgcid:
+            return await message.reply_text(
+                "Tidak ada mentions"
+            )
+        try:
+            tagallgcid.remove(message.chat.id)
+        except Exception:
+            pass
+        await message.reply_text("<b>Stoped.</b>")
