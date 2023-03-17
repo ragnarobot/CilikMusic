d2da77a438fd0223f738a7b41c4bf5df6e1491d4
Grey <95908917+grey423@users.noreply.github.com>
2023-03-17 09:12:46 -0700

    Update settings.py

--- CilikMusic/utils/inline/settings.py
+++ CilikMusic/utils/inline/settings.py
@@ -26,14 +26,11 @@ def setting_markup(_):
             InlineKeyboardButton(
                 text=_["ST_B_3"], callback_data="AU"
             ),
-            InlineKeyboardButton(
-                text=_["ST_B_6"], callback_data="LG"
+           InlineKeyboardButton(
+                text=_["ST_B_5"], callback_data="PM"
             ),
         ],
         [
-            InlineKeyboardButton(
-                text=_["ST_B_5"], callback_data="PM"
-            ),
             InlineKeyboardButton(
                 text=_["ST_B_7"], callback_data="CM"
             ),
